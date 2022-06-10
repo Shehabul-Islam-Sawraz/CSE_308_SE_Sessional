c07278647bcec4497ab4d7fac4a0098b0aa3a3f6
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2022-06-10 08:31:27 +0600

    Problem solved

--- Syntax_Highlighting_Editor/src/Main.java
+++ Syntax_Highlighting_Editor/src/Main.java
@@ -5,7 +5,10 @@ public class Main {
         Editor editor = Editor.getEditor();
         Scanner sc = new Scanner(System.in);
         while(true) {
-            System.out.print("Enter file name: ");
+            System.out.print("Enter file name(Q for exit): ");
+            if(sc.nextLine().equals("Q")){
+                break;
+            }
             String s = sc.nextLine();
             String [] words = s.split("\\.");
             String extension = words[words.length-1];
